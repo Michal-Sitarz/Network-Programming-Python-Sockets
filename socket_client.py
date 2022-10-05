baaa07761b3bc127c42237d3045f06ffc40fe429
Michal-Sitarz <me@michalsitarz.com>
2022-10-05 22:12:00 +0100

    Finish implementing client and server scripts.

--- socket_client.py
+++ socket_client.py
@@ -1,16 +1,53 @@
 import socket
 
+print("[ CLIENT ]")
+
 # SETUP connection
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(("127.0.0.5", 8088))
 print("Connected.")
 
-# Send message to a client
-data = client_socket.recv(1024) # receive
-message = data.decode()
+def get_text(receiving_socket):
+    buffer = ""
+
+    is_socket_open = True
+    while is_socket_open:
+        # read any data from the socket
+        data = receiving_socket.recv(1024)
+
+        # if no data is returned, the socket must be closed
+        if not data:
+            is_socket_open = False
+        
+        # else add the data to the buffer
+        buffer += data.decode()
+
+        # is there a "terminator" character in the buffer?
+        terminator_index = buffer.find("\n")
+        # if index value of terminator is > -1, it means that "\n" must be found/exist
+        while terminator_index > -1:
+            # get the completed/terminated message from the buffer
+            message = buffer[:terminator_index] # all until terminator char
+            # remove this message from the buffer
+            buffer = buffer[terminator_index+1:] # from terminator char till end
+            # yield the message (return a message while continuing the while loop)
+            yield message
+            # is there another terminator in the buffer?
+            terminator_index = buffer.find("\n")
+
+
+# Receive message in a client
+message = next(get_text(client_socket))
+print(message)
+message = next(get_text(client_socket))
 print(message)
 
-# Receive message from a client
+# Using below function will hang both client and server (as while loop gets stuck in infinite)
+# Uncomment it to run it!
+# for message in get_text(client_socket):
+#     print(message)
+
+# Send message back from a client
 message_back = "Client: Roger, roger."
 data = message_back.encode()
 client_socket.send(data)
--- socket_server.py
+++ socket_server.py
@@ -1,5 +1,7 @@
 import socket
 
+print("[ SERVER ]")
+
 # SETUP connection
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(("127.0.0.5", 8088)) # 0.0.0.0 will bind to ALL available IP addresses
@@ -16,11 +18,14 @@ def send_text(sending_socket, text):
     data = text.encode() # standard UTF-8 encoding (each char into byte)
     sending_socket.send(data)
 
-# Receive message from a server
-message = "Server: Thanks for connecting."
+# Send messages from a server
+message = "Server: Thanks for connecting.\nHere's new line message."
+send_text(connection_socket, message)
+message = "Server: Another message here!\nAnd another new line message."
 send_text(connection_socket, message)
 
-# Send message back to server
+
+# Receive message back to server
 data = connection_socket.recv(1024)
 message_back = data.decode()
 print(message_back)
